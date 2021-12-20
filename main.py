d127431476c7655ee6872ef4c0de4d4378366679
Jurgden <82841924+jurgden@users.noreply.github.com>
2021-12-20 22:42:05 +0000

    saving

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import time
+from datetime import datetime as dt
+
 
 
 def get_driver():
@@ -19,11 +21,23 @@ def clean_text(text):
   output = float(text.split(': ')[1])
   return output
 
+def write_file(text):
+  'Write text into text file'
+  filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+  with open(filename, 'w') as file:
+    file.write(text)
+
 
 def main():
   driver = get_driver()
-  time.sleep(2)
-  element = driver.find_element(by='xpath', value='/html/body/div[1]/div/h1[2]')
-  return clean_text(element.text)
+  while True:
+    time.sleep(2)
+    element = driver.find_element(by='xpath', value='/html/body/div[1]/div/h1[2]')
+    text = str(clean_text(element.text))
+    write_file(text)
+    print(text)
 
+
+  
 print(main())
+
